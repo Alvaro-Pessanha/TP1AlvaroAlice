88868c44f125ee49e449493e3009353f4cdaa2f2
Alice016 <aliceribeiro.rl14@gmail.com>
2019-07-04 10:25:42 -0300

    Endereco

---
+++ Gato/src/gato/amazon/Endereco.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gato.amazon;
+
+/**
+ *
+ * @author aluno
+ */
+public class Endereco {
+    private String rua;
+    private int nome;
+    private String cep;
+    private int localizacao;
+    private String complemento;
+    private int numero;
+
+    /**
+     * @return the rua
+     */
+    public String getRua() {
+        return rua;
+    }
+
+    /**
+     * @param rua the rua to set
+     */
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    /**
+     * @return the nome
+     */
+    public int getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(int nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the cep
+     */
+    public String getCep() {
+        return cep;
+    }
+
+    /**
+     * @param cep the cep to set
+     */
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    /**
+     * @return the localizacao
+     */
+    public int getLocalizacao() {
+        return localizacao;
+    }
+
+    /**
+     * @param localizacao the localizacao to set
+     */
+    public void setLocalizacao(int localizacao) {
+        this.localizacao = localizacao;
+    }
+
+    /**
+     * @return the complemento
+     */
+    public String getComplemento() {
+        return complemento;
+    }
+
+    /**
+     * @param complemento the complemento to set
+     */
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    /**
+     * @return the numero
+     */
+    public int getNumero() {
+        return numero;
+    }
+
+    /**
+     * @param numero the numero to set
+     */
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+          
+    
+}
